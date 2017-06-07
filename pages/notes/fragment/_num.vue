7cc2febe6129faf3e0158f349e1a4992c04754cf
LV <mamboer@live.com>
2017-06-07 19:55:06 +0800

    add fragment feature

---
+++ pages/notes/fragment/_num.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="posts clearfix">
+    <template v-for="item in items">
+      <post-mini :post="item"></post-mini>
+    </template>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+</style>
+
+<script>
+import axios from '~plugins/axios'
+import PostMini from '~components/post/Mini.vue'
+export default {
+  layout: 'fragment',
+  validate ({params}) {
+    // must be int
+    return /^\d+$/.test(params.num)
+  },
+  async asyncData ({store, params}) {
+    let langKey = store.state.i18n.curKey
+    let pageNum = params.num
+    let res = await axios.get(`post/list/${langKey}/${pageNum}`)
+    return res.data
+  },
+  components: {
+    PostMini
+  }
+}
+</script>
